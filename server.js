95b6aa7c22e693486151931d8405ee4eb6e69c0f
palak1710 <31029876+palak1710@users.noreply.github.com>
2017-08-19 11:54:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req,res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
 app.get('/article-two', function (req,res) {
     res.send('Article two requested and will be served here.');
 });
@@ -17,9 +21,6 @@ app.get('/article-three', function (req,res) {
     res.send('Article three requested and will be served here.');
 });
 
-app.get('/article-one', function (req,res) {
-    res.send('Article one requested and will be served here.');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
